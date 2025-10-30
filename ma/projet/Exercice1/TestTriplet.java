c4e59c65a479eb8f7abd296878a757b201e6d69f
Salakhal <lakhalsalma18@gmail.com>
2025-10-30 14:52:16 +0100

    first commit

---
+++ ma/projet/Exercice1/TestTriplet.java
@@ -0,0 +1,18 @@
+package ma.projet.Exercice1;
+
+
+	public class TestTriplet {
+	    public static void main(String[] args) {
+	        // Triplet d'entiers
+	        Triplet<Integer> tInt = new Triplet<>(1, 2, 3);
+	        tInt.affiche();  // affiche [1, 2, 3]
+
+	        // Triplet de chaînes
+	        Triplet<String> tStr = new Triplet<>("A", "B", "C");
+	        tStr.affiche();  // affiche [A, B, C]
+
+	        // Utilisation des getters
+	        System.out.println("Second élément de tInt : " + tInt.getSecond());
+	    }
+	}
+
---
+++ ma/projet/Exercice2/TestTripletH.java
@@ -0,0 +1,5 @@
+package ma.projet.Exercice2;
+
+public class TestTripletH {
+
+}
